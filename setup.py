9ae8e64f9cb7a5a73ec81d163db33e97a77fad4a
Petar <petar.v.todorov@gmail.com>
2021-09-17 20:45:43 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='toy-hook',
+      version='0.1',
+      description='toy hook that prints a message on firing',
+      url='http://github.com/pvtodorov/toy_hook',
+      author='Petar Todorov',
+      license='MIT',
+      packages=['toy_hook'],
+      zip_safe=False)
